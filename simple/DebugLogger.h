0cdbe4af1698b9ef922c828f5896da202328d0cd
Erno Pakarinen <codesmith.fi@gmail.com>
2021-06-23 22:56:19 +0300

    Added mutex locking for thread safety

--- simple/DebugLogger.h
+++ simple/DebugLogger.h
@@ -16,10 +16,33 @@
   *
   * Author: Erno Pakarinen
   * Email: codesmith.fi@gmail.com
-  * Date: 25th of May 2021
+  * Date: 23th of June 2021
   *
-  * MIT License
-  * Free to use, modify, copy, what ever. <3
+ * ------------------
+ * CSMV1.1 - Codesmith License
+ * Copyright(c) 1999 - 2021 Erno Pakarinen
+ *
+ *This licence is based on the MIT license model with very few exceptions.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this softwareand associated documentation files(the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and /or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions :
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.Also the original
+ * author shall be credited of the work related to this Software in all
+ * software based on or using this Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+
   */
 #ifndef __DEBUGLOGGER_DEFINED_H__
 #define __DEBUGLOGGER_DEFINED_H__
@@ -29,11 +52,23 @@
 #include <sstream>
 #include <chrono>
 #include <ctime>
+#include <mutex>
 
 namespace codesmith
 {
 	namespace debug
 	{
+		/**
+		 * This mutex is to make logging thread safe.
+		 * Mutex is locked in DebugLogger constructor and released in
+		 * the destructor. Since the usage of the debug logger is through
+		 * the provided macros (see bottom of this header) there should
+		 * not be a possibility of a lock down.
+		 * 
+		 * Feel free to improve if you need to and send me comments please :)
+		 */
+		static std::mutex g_debuglogger_mutex;
+
 		/**
 		 * Defines the supported warning levels for the macro.
 		 * The level defines a warning level text displayed by the macro
@@ -65,7 +100,9 @@ namespace codesmith
 		public:
 			DebugLogger(DebugLogLevel severity = DebugLogLevel::EERROR, bool showtime = true) 
 				: m_buffer(), m_stm{}
-			{				
+			{	
+				g_debuglogger_mutex.lock();
+
 				if (showtime) {
 					make_time();
 					m_buffer << "[" << std::put_time(&m_stm, "%F %T") << "] ";
@@ -93,7 +130,8 @@ namespace codesmith
 
 			// Destructor, causes the debug info to be outputted with new line
 			virtual ~DebugLogger() {
-				std::cerr << m_buffer.str() << std::endl << std::flush;
+				std::cerr << m_buffer.str() << std::endl;
+				g_debuglogger_mutex.unlock();
 			}
 
 			/**
